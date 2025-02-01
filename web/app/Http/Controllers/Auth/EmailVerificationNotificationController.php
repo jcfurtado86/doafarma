4270962b3cceb14bc19ca3ccbb4a13be1cfcc702
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-02-01 04:09:52 -0300

    style: update pint.json and run linter

--- web/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ web/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -12,7 +14,7 @@ class EmailVerificationNotificationController extends Controller
     /**
      * Send a new email verification notification.
      */
-    public function store(Request $request): JsonResponse|RedirectResponse
+    public function store(Request $request): JsonResponse | RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended('/dashboard');

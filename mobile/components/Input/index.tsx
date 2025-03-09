ec92bb9133d37924b29cf1a7be6c9c54d0e46385
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-03-09 01:53:18 -0300

    fix: correct view component casing in Input component

--- mobile/components/Input/index.tsx
+++ mobile/components/Input/index.tsx
@@ -2,8 +2,7 @@ import { TextInput } from 'react-native';
 import { styles } from './styles';
 
 export function Input() {
-  // eslint-disable-next-line prettier/prettier
-  return ( 
+  return (
     <view style={styles.group}>
       <TextInput style={styles.control} />
     </view>

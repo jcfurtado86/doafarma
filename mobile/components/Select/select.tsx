7ae68cd27632b2b1d89b7fb4397c90ebbbb5fb86
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-03-12 12:38:17 -0300

    feat: enhance component flexibility with custom styling options
    Add optional props for custom View and Picker styles, allowing for greater
    customization of the Select component in various UI contexts.

--- mobile/components/Select/select.tsx
+++ mobile/components/Select/select.tsx
@@ -2,22 +2,27 @@ import { styles } from './styles';
 import { Controller, UseControllerProps } from 'react-hook-form';
 import { useState } from 'react';
 import { Picker, PickerProps } from '@react-native-picker/picker';
-import { View } from 'react-native';
+import { StyleProp, TextStyle, View, ViewStyle } from 'react-native';
 
 interface SelectProps {
   formProps: UseControllerProps;
   selectProps: PickerProps;
   children: React.ReactNode;
+  styleView?: StyleProp<ViewStyle>;
+  styleSelect?: StyleProp<TextStyle>;
 }
 
-export function Select({ formProps, selectProps, children }: SelectProps) {
+export function Select({ formProps, selectProps, children, styleView, styleSelect }: SelectProps) {
   const [selectedLanguage, setSelectedLanguage] = useState<string | number | undefined>(undefined);
   const [isFocused, setIsFocused] = useState(false);
   return (
     <Controller
       render={() => (
-        <View style={[styles.selectContainer, isFocused && styles.selectContainerFocused]}>
+        <View
+          style={[styles.selectContainer, isFocused && styles.selectContainerFocused, styleView]}
+        >
           <Picker
+            style={[styleSelect]}
             onFocus={() => setIsFocused(true)}
             onBlur={() => setIsFocused(false)}
             onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}

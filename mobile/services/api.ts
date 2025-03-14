b9227652b16d1b49d0f2efe24cf0f88fbc855226
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-03-13 23:38:27 -0300

    feat: add dotenv configuration for dynamic host IP and update API base URL

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
+
+const HOST_IP = Constants.expoConfig?.extra?.hostIp || '192.168.1.71'; // Fallback para desenvolvimento
+
+const getBaseUrl = () => {
+  const baseUrl =
+    Platform.OS === 'web' ? 'http://localhost:8000/api' : `http://${HOST_IP}:8000/api`;
+
+  console.log('API está usando baseURL:', baseUrl);
+  console.log('Platform.OS:', Platform.OS);
+  return baseUrl;
+};
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: getBaseUrl(),
 });
 
 export default api;

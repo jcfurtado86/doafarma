ee6ba0c34f94efa63f029f4990ce90d52b487d7d
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-03-20 15:57:17 -0300

    fix: update API host configuration and add production env to .gitignore

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const HOST_IP = process.env.EXPO_PUBLIC_HOST_IP || '192.168.0.1'; // Substitua pelo IP correto do seu servidor
+const API_HOST = process.env.EXPO_PUBLIC_API_HOST || '192.168.0.1'; // Substitua pelo IP correto do seu servidor
 
 const getBaseUrl = () => {
-  const baseUrl =
-    Platform.OS === 'web' ? 'http://localhost:8000/api' : `http://${HOST_IP}:8000/api`;
+  if (Platform.OS === 'web') {
+    return 'http://localhost:8000/api';
+  }
 
-  console.log('API está usando baseURL:', baseUrl);
-  console.log('Platform.OS:', Platform.OS);
-  return baseUrl;
+  if (API_HOST.startsWith('http://') || API_HOST.startsWith('https://')) {
+    return `${API_HOST}/api`;
+  }
+
+  return `http://${API_HOST}:8000/api`;
 };
 
 const api = axios.create({

738f4fb1d775c4a6f4507a485890ac1bf8531455
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-03-27 20:03:08 -0300

    feat: add request and response interceptors for authentication

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,4 +1,7 @@
+import { useAuthStore } from '@/stores/authStore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import { router } from 'expo-router';
 import { Platform } from 'react-native';
 
 const API_HOST = process.env.EXPO_PUBLIC_API_HOST || '192.168.0.1'; // Substitua pelo IP correto do seu servidor
@@ -18,5 +21,33 @@ const getBaseUrl = () => {
 const api = axios.create({
   baseURL: getBaseUrl(),
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('auth_token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // 401 Unauthorized error handling
+    if (error.response?.status === 401) {
+      await useAuthStore.getState().logout();
+
+      if (router) {
+        router.replace('/');
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;

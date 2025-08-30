4eb24fadc142d52e11e75ccd884448201fca1b98
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-08-30 19:49:05 -0300

    feat: Add Drug entity with model, migration, and factory
    This commit introduces the `Drug` model, which is a fundamental component for managing medications within the system.
    
    It includes:
    - The database migration to create the `drugs` table with columns for substance, laboratory, registration number, presentation, and stripe color.
    - An Eloquent model to interact with the `drugs` table.
    - A model factory to facilitate testing and database seeding.
    - A comprehensive suite of unit tests to validate the model's behavior, including creation, persistence, updates, and constraints.

---
+++ web/database/factories/DrugFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Drug>
+ */
+class DrugFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'substance'           => $this->faker->word(),
+            'laboratory'          => $this->faker->word(),
+            'registration_number' => $this->faker->unique()->word(),
+            'presentation'        => $this->faker->text(),
+            'stripe_color'        => $this->faker->word(),
+        ];
+    }
+}

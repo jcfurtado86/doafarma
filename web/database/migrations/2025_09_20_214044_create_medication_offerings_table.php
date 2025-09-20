ba058c7ebb33aef344afa672c223795b8a5b52c7
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-09-20 19:41:05 -0300

    feat: add medication offering resource
    This commit introduces the foundational structure for the medication offering feature.
    
    It includes the Eloquent model, database migration, and factory for the `MedicationOffering` entity. Additionally, it sets up the policy for authorization and placeholder form requests for handling creation and updates.

---
+++ web/database/migrations/2025_09_20_214044_create_medication_offerings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medication_offerings', function (Blueprint $table): void {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('drug_id');
+            $table->string('lot_number', 255);
+            $table->date('expires_at');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('drug_id')->references('id')->on('drugs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medication_offerings');
+    }
+};

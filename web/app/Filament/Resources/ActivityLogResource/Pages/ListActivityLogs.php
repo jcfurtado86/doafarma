9fc72a6d8524ffa8bd9c530a74524e2bf3c5802e
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2026-01-31 19:03:26 -0300

    perf: add eager loading to ActivityLogResource
    Fix N+1 query on causer relationship in activity logs listing.

--- web/app/Filament/Resources/ActivityLogResource/Pages/ListActivityLogs.php
+++ web/app/Filament/Resources/ActivityLogResource/Pages/ListActivityLogs.php
@@ -6,13 +6,27 @@ namespace App\Filament\Resources\ActivityLogResource\Pages;
 
 use App\Filament\Resources\ActivityLogResource;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Override;
+use Spatie\Activitylog\Models\Activity;
 
 class ListActivityLogs extends ListRecords
 {
     protected static string $resource = ActivityLogResource::class;
 
+    #[Override]
     protected function getHeaderActions(): array
     {
         return [];
+    }
+
+    /**
+     * @return Builder<Activity>
+     */
+    #[Override]
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()
+            ->with(['causer']);
     }
 }
